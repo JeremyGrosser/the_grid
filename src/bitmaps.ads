25cf483a3785c6ab112f0c16ef602cf8d8adfc91
Jeremy Grosser <jeremy@synack.me>
2021-11-11 17:38:26 -0800

    Yet another unused dep

--- src/bitmaps.ads
+++ src/bitmaps.ads
@@ -3,14 +3,13 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with Picosystem.Screen; use Picosystem.Screen;
-
 package Bitmaps is
-   type Gray is mod 4
-      with Size => 2;
 
    Width  : constant := 8;
    Height : constant := 8;
+
+   type Gray is mod 4
+      with Size => 2;
 
    type Bitmap is array (1 .. Height, 1 .. Width) of Gray
       with Component_Size => 2;
