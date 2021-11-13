dff9c4ee11c98c20e0bb7c7c0fe4ea9ef9bc4c82
Jeremy Grosser <jeremy@synack.me>
2021-11-13 15:24:05 -0800

    explain bitmap format

--- src/bitmaps.ads
+++ src/bitmaps.ads
@@ -10,6 +10,9 @@ package Bitmaps is
    Width  : constant := 8;
    Height : constant := 8;
 
+   --  All bitmaps are stored as row-major, 2-bit per pixel color value. The
+   --  actual display color is determined by a lookup from this value into a
+   --  Graphics.Color_Palette.
    type Bitmap is array (1 .. Height, 1 .. Width) of Graphics.Color_Value
       with Component_Size => 2;
 
